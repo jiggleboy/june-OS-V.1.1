1038635a95b9c8d61d280b86f51cb7583d2e077d
jiggleboy <3375brsilv@bpsstudents.braintreeschools.org>
2026-05-17 09:11:52 -0400

    vmtools.py
    Updated vmtools (now includes a floppy addon)

--- vmtools.py
+++ vmtools.py
@@ -1,7 +1,12 @@
 # The tinkervmtools package is used for
 # help building a VM, not getting a pre-built one. V.1.0
 
+# floppymemtools is used to manage your virtual floppy
+# and emulate one, before you run the OS, just make sure
+# to pre-load data on to it.
+
 ram = []
+floppy = []
 class tinkervmtools:
     def __init__(self):
         pass
@@ -13,3 +18,14 @@ class tinkervmtools:
         return ram[index]
     def clr():
         print("\033[2J\033[H")
+class floppymemtools:
+    def __init__(self):
+        pass
+    def writeToFloppy(data):
+        floppy.append(data)
+    def loadFromFloppy(index):
+        return floppy[index]
+    def popFloppy():
+        return floppy.pop()
+    def peekFromFloppy():
+        return floppy.peek()
